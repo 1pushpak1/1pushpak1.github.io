135a696b17922e25e4567d76f2c314efda9ba943
Pushpak <pushpakkumawat20028@gmail.com>
2023-02-17 23:56:10 +0530

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,19 +126,7 @@ function showProjects(projects) {
     </div>`
     });
     projectsContainer.innerHTML = projectHTML;
- 
- function showCerificates(certificates) {
-    let projectsContainer = document.querySelector("#certificates .box-container");
-    let projectHTML = "";
-  {
-        projectHTML += `
-        <div class="box tilt">
-      <img draggable="false" src="/assets/images/certificates/${certificates.image}.png" alt="caertificates" />
-         
-      
-    </div>`
-    });
-    certificatesContainer.innerHTML = certificatesHTML;
+
     // <!-- tilt js effect starts -->
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
         max: 15,
@@ -165,9 +153,6 @@ fetchData().then(data => {
 fetchData("projects").then(data => {
     showProjects(data);
 });
-    
-fetchData("certificates").then(data => {
-    showCertificates(data);
 
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
@@ -257,8 +242,7 @@ srtop.reveal('.education .box', { interval: 200 });
 
 /* SCROLL PROJECTS */
 srtop.reveal('.work .box', { interval: 200 });
-/* SCROOL CERTIFICATES*/
-srtop.reveal('.certificates .box1', { interval: 200 });
+
 /* SCROLL EXPERIENCE */
 srtop.reveal('.experience .timeline', { delay: 400 });
 srtop.reveal('.experience .timeline .container', { interval: 400 });
