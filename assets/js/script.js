3bd3c62597f913066eb1d77432875185b12256ce
Pushpak <pushpakkumawat20028@gmail.com>
2023-02-18 20:50:58 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
     $("#contact-form").submit(function (event) {
         emailjs.init("iqdapiR1PhZ9ZTfbE");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_ohk4jj8', 'template_2tc9m9v', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
