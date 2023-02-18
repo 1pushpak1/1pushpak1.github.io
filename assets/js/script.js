6d2f38a0fd617a6626724ca00381ea5dc6a298f6
Pushpak <pushpakkumawat20028@gmail.com>
2023-02-18 21:14:35 +0530

    finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -229,18 +229,18 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/63abc99dc2f1ac1e202a6fd8/1glbgn4s0';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
+// // Start of Tawk.to Live Chat
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//     s1.async=true;
+//     s1.src='https://embed.tawk.to/63abc99dc2f1ac1e202a6fd8/1glbgn4s0';
+//     s1.charset='UTF-8';
+//     s1.setAttribute('crossorigin','*');
+//     s0.parentNode.insertBefore(s1,s0);
 
-})();
-// End of Tawk.to Live Chat
+// })();
+// // End of Tawk.to Live Chat
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
